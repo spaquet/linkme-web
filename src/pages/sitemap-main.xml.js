bae51d1369594789fd63c2e446315bf89757e4d2
Stephane Paquet <176050+spaquet@users.noreply.github.com>
2025-05-08 11:04:24 -0700

    adding tailing /

--- src/pages/sitemap-main.xml.js
+++ src/pages/sitemap-main.xml.js
@@ -2,8 +2,8 @@
 export async function GET() {
     const siteUrl = 'https://getlinkme.app';
     const pages = [
-      '', '/about', '/features', '/pricing', '/contact-sales',
-      '/privacy', '/terms-of-service', '/cookies',
+      '', '/about/', '/features', '/pricing/', '/contact-sales/',
+      '/privacy/', '/terms-of-service/', '/cookies/', '/faq/', '/careers/',
     ];
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
